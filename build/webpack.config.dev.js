6e8c7ace7ddb1a065dbba1f0a13d572842d8ea2c
SYS-20180628GBX\Administrator <601526@syswin.com>
2018-07-13 11:57:47 +0800

    babel 配置

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -39,6 +39,10 @@ module.exports = {
             {
                 test: /\.vue$/,
                 use: ['vue-loader']
+            },
+            {
+                test: /\.js$/,
+                use: ['babel-loader']
             }
         ]
     }
